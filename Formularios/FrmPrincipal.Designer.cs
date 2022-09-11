ba680baac6ea45c4fa6ae88a5ba9533a5af1ea41
Vaneg <Vaneg@HP-1GR12LA>
2022-09-11 15:44:22 -0600

    Agregar archivos de proyecto.

---
+++ Formularios/FrmPrincipal.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Duisv.Formularios
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.BtnEditarPerfil = new System.Windows.Forms.Button();
+            this.BtnAgregarUsuario = new System.Windows.Forms.Button();
+            this.BtnEliminarUsuario = new System.Windows.Forms.Button();
+            this.BtnRegistrar = new System.Windows.Forms.Button();
+            this.BtnEditar = new System.Windows.Forms.Button();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.PnlOpciones = new System.Windows.Forms.Panel();
+            this.PBxLogotipo = new System.Windows.Forms.PictureBox();
+            this.PBxAvatar = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PBxCerrar = new System.Windows.Forms.PictureBox();
+            this.PBxMinimizar = new System.Windows.Forms.PictureBox();
+            this.PnlOpciones.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBxLogotipo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBxAvatar)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBxCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBxMinimizar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnEditarPerfil
+            // 
+            this.BtnEditarPerfil.Location = new System.Drawing.Point(12, 89);
+            this.BtnEditarPerfil.Name = "BtnEditarPerfil";
+            this.BtnEditarPerfil.Size = new System.Drawing.Size(129, 23);
+            this.BtnEditarPerfil.TabIndex = 0;
+            this.BtnEditarPerfil.Text = "Editar perfil";
+            this.BtnEditarPerfil.UseVisualStyleBackColor = true;
+            // 
+            // BtnAgregarUsuario
+            // 
+            this.BtnAgregarUsuario.Location = new System.Drawing.Point(12, 144);
+            this.BtnAgregarUsuario.Name = "BtnAgregarUsuario";
+            this.BtnAgregarUsuario.Size = new System.Drawing.Size(129, 23);
+            this.BtnAgregarUsuario.TabIndex = 0;
+            this.BtnAgregarUsuario.Text = "Agregar usuario";
+            this.BtnAgregarUsuario.UseVisualStyleBackColor = true;
+            // 
+            // BtnEliminarUsuario
+            // 
+            this.BtnEliminarUsuario.Location = new System.Drawing.Point(12, 173);
+            this.BtnEliminarUsuario.Name = "BtnEliminarUsuario";
+            this.BtnEliminarUsuario.Size = new System.Drawing.Size(129, 23);
+            this.BtnEliminarUsuario.TabIndex = 0;
+            this.BtnEliminarUsuario.Text = "Eliminar usuario";
+            this.BtnEliminarUsuario.UseVisualStyleBackColor = true;
+            // 
+            // BtnRegistrar
+            // 
+            this.BtnRegistrar.Location = new System.Drawing.Point(12, 227);
+            this.BtnRegistrar.Name = "BtnRegistrar";
+            this.BtnRegistrar.Size = new System.Drawing.Size(129, 23);
+            this.BtnRegistrar.TabIndex = 0;
+            this.BtnRegistrar.Text = "Registrar";
+            this.BtnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // BtnEditar
+            // 
+            this.BtnEditar.Location = new System.Drawing.Point(12, 256);
+            this.BtnEditar.Name = "BtnEditar";
+            this.BtnEditar.Size = new System.Drawing.Size(129, 23);
+            this.BtnEditar.TabIndex = 0;
+            this.BtnEditar.Text = "Editar";
+            this.BtnEditar.UseVisualStyleBackColor = true;
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(12, 285);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(129, 23);
+            this.BtnBuscar.TabIndex = 0;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BtnSalir.Location = new System.Drawing.Point(12, 587);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(129, 23);
+            this.BtnSalir.TabIndex = 0;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            // 
+            // PnlOpciones
+            // 
+            this.PnlOpciones.BackColor = System.Drawing.Color.White;
+            this.PnlOpciones.Controls.Add(this.BtnSalir);
+            this.PnlOpciones.Controls.Add(this.PBxLogotipo);
+            this.PnlOpciones.Controls.Add(this.BtnBuscar);
+            this.PnlOpciones.Controls.Add(this.BtnEditar);
+            this.PnlOpciones.Controls.Add(this.BtnRegistrar);
+            this.PnlOpciones.Controls.Add(this.BtnEliminarUsuario);
+            this.PnlOpciones.Controls.Add(this.BtnAgregarUsuario);
+            this.PnlOpciones.Controls.Add(this.BtnEditarPerfil);
+            this.PnlOpciones.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PnlOpciones.Location = new System.Drawing.Point(0, 0);
+            this.PnlOpciones.Name = "PnlOpciones";
+            this.PnlOpciones.Size = new System.Drawing.Size(157, 622);
+            this.PnlOpciones.TabIndex = 1;
+            // 
+            // PBxLogotipo
+            // 
+            this.PBxLogotipo.Location = new System.Drawing.Point(12, 12);
+            this.PBxLogotipo.Name = "PBxLogotipo";
+            this.PBxLogotipo.Size = new System.Drawing.Size(129, 50);
+            this.PBxLogotipo.TabIndex = 0;
+            this.PBxLogotipo.TabStop = false;
+            // 
+            // PBxAvatar
+            // 
+            this.PBxAvatar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PBxAvatar.Image = ((System.Drawing.Image)(resources.GetObject("PBxAvatar.Image")));
+            this.PBxAvatar.Location = new System.Drawing.Point(157, 70);
+            this.PBxAvatar.Name = "PBxAvatar";
+            this.PBxAvatar.Size = new System.Drawing.Size(926, 552);
+            this.PBxAvatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBxAvatar.TabIndex = 0;
+            this.PBxAvatar.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.PBxMinimizar);
+            this.panel1.Controls.Add(this.PBxCerrar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(157, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(926, 70);
+            this.panel1.TabIndex = 2;
+            // 
+            // PBxCerrar
+            // 
+            this.PBxCerrar.Location = new System.Drawing.Point(785, 12);
+            this.PBxCerrar.Name = "PBxCerrar";
+            this.PBxCerrar.Size = new System.Drawing.Size(129, 50);
+            this.PBxCerrar.TabIndex = 0;
+            this.PBxCerrar.TabStop = false;
+            // 
+            // PBxMinimizar
+            // 
+            this.PBxMinimizar.Location = new System.Drawing.Point(650, 12);
+            this.PBxMinimizar.Name = "PBxMinimizar";
+            this.PBxMinimizar.Size = new System.Drawing.Size(129, 50);
+            this.PBxMinimizar.TabIndex = 0;
+            this.PBxMinimizar.TabStop = false;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 622);
+            this.Controls.Add(this.PBxAvatar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PnlOpciones);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DUISV";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.PnlOpciones.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PBxLogotipo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBxAvatar)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PBxCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBxMinimizar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnEditarPerfil;
+        private System.Windows.Forms.Button BtnAgregarUsuario;
+        private System.Windows.Forms.Button BtnEliminarUsuario;
+        private System.Windows.Forms.Button BtnRegistrar;
+        private System.Windows.Forms.Button BtnEditar;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Panel PnlOpciones;
+        private System.Windows.Forms.PictureBox PBxLogotipo;
+        private System.Windows.Forms.PictureBox PBxAvatar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox PBxMinimizar;
+        private System.Windows.Forms.PictureBox PBxCerrar;
+    }
+}
