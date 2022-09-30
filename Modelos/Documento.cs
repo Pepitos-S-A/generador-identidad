909ade547c3cdba9c85875a5a52f8823218eb1ef
Jonathan Vanegas <srquike@outlook.com>
2022-09-30 09:47:58 -0600

    Se creo el modelo para los Documentos Unicos de Identidad

---
+++ Modelos/Documento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Duisv.Modelos
+{
+    internal class Documento
+    {
+        public int DocumentoId { get; set; }
+        public DateTime FechaExpedicion { get; set; }
+        public string MunicipioExpedicion { get; set; } = string.Empty;
+        public string DepartamentoExpedicion { get; set; } = string.Empty;
+        public DateTime FechaExpiracion { get; set; }
+        public string Numero { get; set; } = string.Empty;
+        public string TipoTramite { get; set; } = string.Empty;
+        public string CodigoZona { get; set; } = string.Empty;
+        public string NumeroFolio { get; set; } = string.Empty;
+    }
+}
