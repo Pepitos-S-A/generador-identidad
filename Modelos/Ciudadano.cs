cf9895dc14b8dad6798c9d170c3d281f8680ecb1
Jonathan Vanegas <srquike@outlook.com>
2022-09-30 09:55:41 -0600

    Se agrego el modelo para los Ciudadanos

---
+++ Modelos/Ciudadano.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Duisv.Modelos
+{
+    internal class Ciudadano
+    {
+        public int CiudadanoId { get; set; }
+        public string Nombres { get; set; } = string.Empty;
+        public string Apellidos { get; set; } = string.Empty;
+        public string ConocidoPor { get; set; } = string.Empty;
+        public string Genero { get; set; } = string.Empty;
+        public string CiudadanoPor { get; set; } = string.Empty;
+        public DateTime FechaNacimiento { get; set; }
+        public string Residencia { get; set; } = string.Empty;
+        public string EstadoFamiliar { get; set; } = string.Empty;
+        public string Profesion { get; set; } = string.Empty;
+        public string TipoSangre { get; set; } = string.Empty;
+        public string NombreMadre { get; set; } = string.Empty;
+        public string NombrePadre { get; set; } = string.Empty;
+        public string NombreConyugue { get; set; } = string.Empty;
+        public string MunicipioNacimiento { get; set; } = string.Empty;
+        public string DepartamentoNacimiento { get; set; } = string.Empty;
+        public string MunicipioResidencia { get; set; } = string.Empty;
+        public string DepartamentoResidencia { get; set; } = string.Empty;
+        public int? DocumentoId { get; set; }
+        public string NumeroDocumento { get; set; } = string.Empty;
+    }
+}
