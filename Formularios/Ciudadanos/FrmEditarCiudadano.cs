df834920acbb26f9bd020b85b5fc835f70e7d4d2
Jonathan Vanegas <srquike@outlook.com>
2022-10-11 15:18:07 -0600

    Se agrego el formulario para editar los datos del ciudadano

---
+++ Formularios/Ciudadanos/FrmEditarCiudadano.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using Duisv.Modelos;
+using Duisv.Servicios;
+
+namespace Duisv.Formularios.Ciudadanos
+{
+    public partial class FrmEditarCiudadano : Form
+    {
+        private readonly CiudadanoServicio _ciudadanoServicio;
+
+        public FrmEditarCiudadano(int ciudadanoId)
+        {
+            InitializeComponent();
+
+            _ciudadanoServicio = new CiudadanoServicio();
+
+            var ciudadano = _ciudadanoServicio.ObtenerCiudadanoPorId(ciudadanoId);
+
+            if (ciudadano != null)
+            {
+                ciudadanoBindingSource.DataSource = ciudadano;
+            }
+        }
+
+        private void FrmEditarCiudadano_Load(object sender, EventArgs e)
+        {
+            var nombres = ((Ciudadano)ciudadanoBindingSource.Current).Nombres;
+            var apellidos = ((Ciudadano)ciudadanoBindingSource.Current).Apellidos;
+
+            LblTituloVentana.Text = $"Editar datos del ciudadano {nombres.ToUpper()} {apellidos.ToUpper()}";
+        }
+    }
+}
