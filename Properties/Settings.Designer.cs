9774e6a56bb91afb5f5981a5b316e8b0bb15a6c1
Jonathan Vanegas <srquike@outlook.com>
2022-11-03 19:50:57 -0600

    Se cambio el servidor en la cadena de conexion

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace Duisv.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=HP-1GR12LA\\SQLEXPRESS;Database=DuisvDb;Trusted_Connection=True;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=HP-AN025LA\\SQLEXPRESS;Database=DuisvDb;Trusted_Connection=True;")]
         public string SqlServerCadenaConexion {
             get {
                 return ((string)(this["SqlServerCadenaConexion"]));
