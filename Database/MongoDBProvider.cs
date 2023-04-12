748350282a004e380d17efa046a18bad82459743
srquike <srquike@outlook.com>
2023-04-12 16:10:02 -0600

    Se agrego el proveedor de objetos de MongoDB

---
+++ Database/MongoDBProvider.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+
+namespace Duisv.Database
+{
+    internal class MongoDBProvider
+    {
+        private readonly MongoClient _client;
+
+        public MongoDBProvider()
+        {
+            _client = new MongoClient(Properties.Settings.Default.MongoDBConnectionString);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string databaseName, string collectionName)
+        {
+            return GetDatabase(databaseName).GetCollection<T>(collectionName);
+        }
+
+        private IMongoDatabase GetDatabase(string databaseName)
+        {
+            return _client.GetDatabase(databaseName);
+        }
+    }
+}
