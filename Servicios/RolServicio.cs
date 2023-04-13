3266b05eae5b6c5be85ee4612f17a915633961a5
srquike <srquike@outlook.com>
2023-04-13 11:36:41 -0600

    Se implementaron los metodos para utilizar MongoDB

--- Servicios/RolServicio.cs
+++ Servicios/RolServicio.cs
@@ -1,51 +1,62 @@
-﻿using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
+﻿using Duisv.Database;
 using Duisv.Modelos;
+using MongoDB.Driver;
+using System.Collections.Generic;
 
 namespace Duisv.Servicios
 {
     internal class RolServicio
     {
-        private readonly string _cadenaConexion;
+        //private readonly string _cadenaConexion;
+        private readonly MongoDBProvider _provider;
+        private readonly IMongoCollection<Rol> _roles;
 
         public RolServicio()
         {
-            _cadenaConexion = Properties.Settings.Default.SqlServerCadenaConexion;
+            //_cadenaConexion = Properties.Settings.Default.SqlServerCadenaConexion;
+            _provider = new MongoDBProvider();
+            _roles = _provider.GetCollection<Rol>("roles");
         }
 
         public List<Rol> ObtenerListaRoles()
         {
-            var roles = new List<Rol>();
-
-            using (var conexion = new SqlConnection(_cadenaConexion))
-            {
-                if (conexion.State != ConnectionState.Open)
-                {
-                    conexion.Open();
-
-                    using (var comando = new SqlCommand("ObtenerListaRoles", conexion))
-                    {
-                        comando.CommandType = CommandType.StoredProcedure;
-
-                        using (var lector = comando.ExecuteReader())
-                        {
-                            while (lector.Read())
-                            {
-                                var rol = new Rol
-                                {
-                                    RolId = lector["RolId"] as int?,
-                                    Nombre = lector["Nombre"].ToString()
-                                };
-
-                                roles.Add(rol);
-                            }
-                        }
-                    }
-                }
-            }
-
-            return roles;
+            return _roles.Find(x => true).ToList();
         }
+
+        #region Métodos con ADO.NET y SQL Server
+        //public List<Rol> ObtenerListaRoles()
+        //{
+        //    var roles = new List<Rol>();
+
+        //    using (var conexion = new SqlConnection(_cadenaConexion))
+        //    {
+        //        if (conexion.State != ConnectionState.Open)
+        //        {
+        //            conexion.Open();
+
+        //            using (var comando = new SqlCommand("ObtenerListaRoles", conexion))
+        //            {
+        //                comando.CommandType = CommandType.StoredProcedure;
+
+        //                using (var lector = comando.ExecuteReader())
+        //                {
+        //                    while (lector.Read())
+        //                    {
+        //                        var rol = new Rol
+        //                        {
+        //                            RolId = lector["RolId"] as int?,
+        //                            Nombre = lector["Nombre"].ToString()
+        //                        };
+
+        //                        roles.Add(rol);
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    return roles;
+        //}
+        #endregion
     }
 }
