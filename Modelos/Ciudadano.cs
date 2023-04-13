0487043f9610ea28a5f2ea943a1f1f1422d212b7
srquike <srquike@outlook.com>
2023-04-13 16:43:33 -0600

    Se agregaron los atributos de serializacion de MongoDB

--- Modelos/Ciudadano.cs
+++ Modelos/Ciudadano.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 
 namespace Duisv.Modelos
 {
@@ -7,10 +9,19 @@ namespace Duisv.Modelos
         private string genero = string.Empty;
         private string tipoSangre = string.Empty;
 
+        [BsonId]
         public int CiudadanoId { get; set; }
+
+        [BsonElement("nombres")]
         public string Nombres { get; set; } = string.Empty;
+
+        [BsonElement("apellidos")]
         public string Apellidos { get; set; } = string.Empty;
+
+        [BsonElement("conocido_por")]
         public string ConocidoPor { get; set; } = string.Empty;
+
+        [BsonElement("genero")]
         public string Genero
         {
             get { return genero; }
@@ -26,11 +37,24 @@ namespace Duisv.Modelos
                 }
             }
         }
+
+        [BsonElement("ciudadano_por")]
         public string CiudadanoPor { get; set; } = string.Empty;
+
+        [BsonElement("fecha_nacimiento")]
+        [BsonRepresentation(BsonType.DateTime)]
         public DateTime? FechaNacimiento { get; set; }
+
+        [BsonElement("residencia")]
         public string Residencia { get; set; } = string.Empty;
+
+        [BsonElement("estado_familiar")]
         public string EstadoFamiliar { get; set; } = string.Empty;
+
+        [BsonElement("profesion")]
         public string Profesion { get; set; } = string.Empty;
+
+        [BsonElement("tipo_sangre")]
         public string TipoSangre 
         { 
             get
@@ -56,14 +80,33 @@ namespace Duisv.Modelos
                 }
             }
         }
+
+        [BsonElement("nombre_madre")]
         public string NombreMadre { get; set; } = string.Empty;
+
+        [BsonElement("nombre_padre")]
         public string NombrePadre { get; set; } = string.Empty;
+
+        [BsonElement("nombre_conyugue")]
         public string NombreConyugue { get; set; } = string.Empty;
+
+        [BsonElement("municipio_nacimiento")]
         public string MunicipioNacimiento { get; set; } = string.Empty;
+
+        [BsonElement("departamento_nacimiento")]
         public string DepartamentoNacimiento { get; set; } = string.Empty;
+
+        [BsonElement("municipio_residencia")]
         public string MunicipioResidencia { get; set; } = string.Empty;
+
+        [BsonElement("departamento_residencia")]
         public string DepartamentoResidencia { get; set; } = string.Empty;
+
+        [BsonElement("documento_id")]
+        [BsonRepresentation(BsonType.Int32)]
         public int? DocumentoId { get; set; }
+
+        [BsonElement("numero_documento")]
         public string NumeroDocumento { get; set; } = string.Empty;
     }
 }
