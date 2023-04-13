039224cba42a6e6f154bfbe90d70998b5ee49367
srquike <srquike@outlook.com>
2023-04-13 16:45:20 -0600

    Se agregaron los atributos de serializacion de MongoDB

--- Modelos/Departamento.cs
+++ Modelos/Departamento.cs
@@ -1,8 +1,13 @@
-﻿namespace Duisv.Modelos
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Duisv.Modelos
 {
     internal class Departamento
     {
+        [BsonId]
         public int DepartamentoId { get; set; } = 0;
+
+        [BsonElement("nombre")]
         public string Nombre { get; set; } = string.Empty;
     }
 }
