b4172973b47769a0db8dffb7cc0f6a7b4f548bff
srquike <srquike@outlook.com>
2023-04-13 10:36:33 -0600

    Revert "Se implementaron los metodos para usar MongoDB"
    This reverts commit cd98af004d31d1cd29566d51ed379018054d1673.

--- Servicios/DepartamentoServicio.cs
+++ Servicios/DepartamentoServicio.cs
@@ -2,62 +2,51 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using Duisv.Database;
 using Duisv.Modelos;
-using MongoDB.Driver;
 
 namespace Duisv.Servicios
 {
     internal class DepartamentoServicio
     {
-        //private readonly string _cadenaConexion;
-        private readonly MongoDBProvider _provider;
-        private readonly IMongoCollection<Departamento> _collection;
+        private readonly string _cadenaConexion;
 
         public DepartamentoServicio()
         {
-            //_cadenaConexion = Properties.Settings.Default.SqlServerCadenaConexion;
-            _provider = new MongoDBProvider();
-            _collection = _provider.GetCollection<Departamento>("departamentos");
+            _cadenaConexion = Properties.Settings.Default.SqlServerCadenaConexion;
         }
 
         public List<Departamento> ObtenerListaDepartamentos()
         {
-            return _collection.Find(x => true).ToList();
+            var departamentos = new List<Departamento>();
+
+            using (var conexion = new SqlConnection(_cadenaConexion))
+            {
+                if (conexion.State != ConnectionState.Open)
+                {
+                    conexion.Open();
+
+                    using (var comando = new SqlCommand("ObtenerListaDepartamentos", conexion))
+                    {
+                        comando.CommandType = CommandType.StoredProcedure;
+
+                        using (var lector = comando.ExecuteReader())
+                        {
+                            while (lector.Read())
+                            {
+                                var departamento = new Departamento
+                                {
+                                    DepartamentoId = Convert.ToInt32(lector["DepartamentoId"].ToString()),
+                                    Nombre = lector["Nombre"].ToString()
+                                };
+
+                                departamentos.Add(departamento);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return departamentos;
         }
-
-        //public List<Departamento> ObtenerListaDepartamentos()
-        //{
-        //    var departamentos = new List<Departamento>();
-
-        //    using (var conexion = new SqlConnection(_cadenaConexion))
-        //    {
-        //        if (conexion.State != ConnectionState.Open)
-        //        {
-        //            conexion.Open();
-
-        //            using (var comando = new SqlCommand("ObtenerListaDepartamentos", conexion))
-        //            {
-        //                comando.CommandType = CommandType.StoredProcedure;
-
-        //                using (var lector = comando.ExecuteReader())
-        //                {
-        //                    while (lector.Read())
-        //                    {
-        //                        var departamento = new Departamento
-        //                        {
-        //                            DepartamentoId = Convert.ToInt32(lector["DepartamentoId"].ToString()),
-        //                            Nombre = lector["Nombre"].ToString()
-        //                        };
-
-        //                        departamentos.Add(departamento);
-        //                    }
-        //                }
-        //            }
-        //        }
-        //    }
-
-        //    return departamentos;
-        //}
     }
 }
