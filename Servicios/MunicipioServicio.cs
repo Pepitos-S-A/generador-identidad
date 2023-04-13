a94a64e32a4b87a05e89518f1bd1a7683054b2c5
srquike <srquike@outlook.com>
2023-04-13 11:32:02 -0600

    Se implementaron los metodos para utilizar MongoDB

--- Servicios/MunicipioServicio.cs
+++ Servicios/MunicipioServicio.cs
@@ -1,57 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Duisv.Database;
 using Duisv.Modelos;
+using MongoDB.Driver;
+using System.Collections.Generic;
 
 namespace Duisv.Servicios
 {
     internal class MunicipioServicio
     {
-        private readonly string _cadenaConexion;
+        //private readonly string _cadenaConexion;
+        private readonly MongoDBProvider _provider;
+        private readonly IMongoCollection<Municipio> _municipios;
 
         public MunicipioServicio()
         {
-            _cadenaConexion = Properties.Settings.Default.SqlServerCadenaConexion;
+            //_cadenaConexion = Properties.Settings.Default.SqlServerCadenaConexion;
+            _provider = new MongoDBProvider();
+            _municipios = _provider.GetCollection<Municipio>("municipios");
         }
 
         public List<Municipio> ObtenerListaMunicipiosPorDepartamentoId(int departamentoId)
         {
-            var municipios = new List<Municipio>();
-
-            using (var conexion = new SqlConnection(_cadenaConexion))
-            {
-                if (conexion.State != ConnectionState.Open)
-                {
-                    conexion.Open();
-
-                    using (var comando = new SqlCommand("ObtenerListaMunicipiosPorDepartamentoId", conexion))
-                    {
-                        comando.CommandType = CommandType.StoredProcedure;
-                        comando.Parameters.AddWithValue("@DepartamentoId", departamentoId);
-
-                        using (var lector = comando.ExecuteReader())
-                        {
-                            while (lector.Read())
-                            {
-                                var municipio = new Municipio
-                                {
-                                    MunicipioId = Convert.ToInt32(lector["MunicipioId"].ToString()),
-                                    Nombre = lector["Nombre"].ToString(),
-                                    DepartamentoId = Convert.ToInt32(lector["DepartamentoId"].ToString())
-                                };
-
-                                municipios.Add(municipio);
-                            }
-                        }
-                    }
-                }
-            }
-
-            return municipios;
+            return _municipios.Find(x => true).ToList();
         }
+
+        #region Métodos con ADO.NET y SQL Server
+        //public List<Municipio> ObtenerListaMunicipiosPorDepartamentoId(int departamentoId)
+        //{
+        //    var municipios = new List<Municipio>();
+
+        //    using (var conexion = new SqlConnection(_cadenaConexion))
+        //    {
+        //        if (conexion.State != ConnectionState.Open)
+        //        {
+        //            conexion.Open();
+
+        //            using (var comando = new SqlCommand("ObtenerListaMunicipiosPorDepartamentoId", conexion))
+        //            {
+        //                comando.CommandType = CommandType.StoredProcedure;
+        //                comando.Parameters.AddWithValue("@DepartamentoId", departamentoId);
+
+        //                using (var lector = comando.ExecuteReader())
+        //                {
+        //                    while (lector.Read())
+        //                    {
+        //                        var municipio = new Municipio
+        //                        {
+        //                            MunicipioId = Convert.ToInt32(lector["MunicipioId"].ToString()),
+        //                            Nombre = lector["Nombre"].ToString(),
+        //                            DepartamentoId = Convert.ToInt32(lector["DepartamentoId"].ToString())
+        //                        };
+
+        //                        municipios.Add(municipio);
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    return municipios;
+        //}
+        #endregion
     }
 }
