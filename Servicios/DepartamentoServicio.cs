7eafb0f1be41969d0301dbe257e5b27a89db45a5
srquike <srquike@outlook.com>
2023-04-13 10:40:21 -0600

    Sr quitaron dependecias sin usar

--- Servicios/DepartamentoServicio.cs
+++ Servicios/DepartamentoServicio.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using Duisv.Database;
+﻿using Duisv.Database;
 using Duisv.Modelos;
 using MongoDB.Driver;
+using System.Collections.Generic;
 
 namespace Duisv.Servicios
 {
